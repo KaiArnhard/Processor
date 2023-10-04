9fa6cec56e438e88746b47b22ace88f059fcbbe4
Kai Arnhard <zadorikars@gmail.com>
2023-10-04 22:30:29 +0300

    small fix

---
+++ calculator/include/command.h
@@ -0,0 +1,21 @@
+#if !defined(COMMAND_H)
+#define COMMAND_H
+
+enum Commands {
+    HLT        = -1,
+    STACK_PUSH =  0,
+    STACK_IN   =  1,
+    STACK_POP  =  2,
+    ADD        =  3,
+    SUB        =  4,
+    DIV        =  5,
+    MUL        =  6,
+    SQRT       =  7,
+    SIN        =  8,
+    COS        =  9,
+    OUT        =  10 
+};
+
+int assembly(const char* fname1, const char* fname2);
+
+#endif // COMMAND_H
